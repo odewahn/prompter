1052c2d8847c3d28fd7ae88bb2c8c61aa824062d
Andrew Odewahn <odewahn@oreilly.com>
2024-12-31 20:45:15 -0500

    feat: Add environment configuration module

---
+++ src/environment.py
@@ -0,0 +1,31 @@
+# This is a class that is used to manage environment variables in the repl and webapp
+# It allows you to set and get environment variables from a dictionary
+
+
+class Environment:
+    def __init__(self):
+        self.env = {}
+
+    def set(self, key, value):
+        self.env[key] = value
+
+    def get(self, key):
+        return self.env.get(key)
+
+    def clear(self):
+        self.env.clear()
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, value):
+        self.set(key, value)
+
+    def __contains__(self, key):
+        return key in self.env
+
+    def __repr__(self):
+        return repr(self.env)
+
+    def __str__(self):
+        return str(self.env)
